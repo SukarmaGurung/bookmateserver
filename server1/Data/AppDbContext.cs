16a4cbef2ab3f0f30836e3e3a483582bc5642e25
SukarmaGurung <sukarmagurung20@gmail.com>
2025-05-13 14:14:53 +0000

    remove announcement

--- server1/Data/AppDbContext.cs
+++ server1/Data/AppDbContext.cs
@@ -9,7 +9,7 @@ public class AppDbContext: DbContext
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
-        public DbSet<Announcement> Announcements { get; set; }
+       
 
         public DbSet<Book> Books { get; set; }
 
